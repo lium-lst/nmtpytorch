67e664e3fc6f5352f3f62fc6aeb38ba1fb474489
Ozan Caglayan <ozancag@gmail.com>
2018-01-25 12:21:28 +0100

    layers.__init: update

--- nmtpytorch/layers/__init__.py
+++ nmtpytorch/layers/__init__.py
@@ -1,4 +1,7 @@
 from .attention import Attention
 from .ff import FF
+from .fusion import Fusion
+from .image_encoder import ImageEncoder
 from .text_encoder import TextEncoder
 from .cond_decoder import ConditionalDecoder
+from .condmm_decoder import ConditionalMMDecoder
