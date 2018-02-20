368eb77f1215ecd6d0d3e435967911695af2c17f
Ozan Caglayan <ozancag@gmail.com>
2018-02-20 14:22:36 +0100

    vocabulary: add explicit_eos support

--- nmtpytorch/vocabulary.py
+++ nmtpytorch/vocabulary.py
@@ -1,10 +1,8 @@
 # -*- coding: utf-8 -*-
-
 import json
 import pathlib
+import warnings
 from collections import OrderedDict
-
-from .utils.misc import get_language
 
 
 class Vocabulary(object):
@@ -15,23 +13,21 @@ class Vocabulary(object):
               "<eos>": 2,
               "<unk>": 3}
 
-    def __init__(self, vocab, lang=None):
-        self.vocab = str(pathlib.Path(vocab).expanduser())
-        self.lang = lang
+    def __init__(self, vocab, name):
+        self.vocab = pathlib.Path(vocab).expanduser()
+        self.name = name
         self._map = None
         self._imap = None
         self._allmap = None
         self.n_tokens = None
 
-        if self.lang is None:
-            self.lang = get_language(self.vocab)
-
         self._map = json.load(open(self.vocab))
 
         # Sanity check for placeholder tokens
         for tok, idx in self.TOKENS.items():
-            assert self._map.get(tok, -1) == idx, \
-                "%s not found in vocabulary." % tok
+            if self._map.get(tok, -1) != idx:
+                warnings.warn('{} not found in {}'.format(
+                    tok, self.vocab.name))
 
         # Set # of tokens
         self.n_tokens = len(self._map)
@@ -53,7 +49,8 @@ class Vocabulary(object):
     def __len__(self):
         return len(self._map)
 
-    def sent_to_idxs(self, line, limit=0, explicit_bos=False):
+    def sent_to_idxs(self, line, limit=0, explicit_bos=False,
+                     explicit_eos=True):
         """Convert from list of strings to list of token indices."""
         tidxs = []
 
@@ -67,8 +64,9 @@ class Vocabulary(object):
             tidxs = [tidx if tidx < limit else self.TOKENS["<unk>"]
                      for tidx in tidxs]
 
-        # Append explicit <eos>
-        tidxs.append(self.TOKENS["<eos>"])
+        if explicit_eos:
+            # Append <eos>
+            tidxs.append(self.TOKENS["<eos>"])
 
         return tidxs
 
@@ -96,14 +94,5 @@ class Vocabulary(object):
         return results
 
     def __repr__(self):
-        return "Vocabulary of %d items (lang=%s)" % (self.n_tokens,
-                                                     self.lang)
-
-
-if __name__ == '__main__':
-    vocab_file = "data/bpe/train.norm.tok.lc.bpe10000.vocab.en"
-    v = Vocabulary(vocab_file, lang='en')
-
-    sent = 'he is a good man .'
-    idxs = v.sent_to_idxs(sent)
-    assert v.idxs_to_sent(idxs) == sent, "Test 1 failed."
+        return "Vocabulary of %d items (name=%s)" % (self.n_tokens,
+                                                     self.name)
