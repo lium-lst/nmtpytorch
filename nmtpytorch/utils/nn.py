cb05697fee99a7ae5550b1b8aeb914fa9f216da0
Ozan Caglayan <ozancag@gmail.com>
2018-02-20 14:34:00 +0100

    utils.nn: remove get_network_topology, add tile_ctx_dict

--- nmtpytorch/utils/nn.py
+++ nmtpytorch/utils/nn.py
@@ -4,10 +4,26 @@ import numpy as np
 from ..config import FLOAT
 
 
-def set_learnable(module, value=False):
+def set_learnable(module, value=False, layer_names=''):
     """Disables updating the weights of CNN to use it as feature extractor."""
-    for param in module.parameters():
-        param.requires_grad = value
+    # Can be empty or not
+    layer_names = tuple(layer_names.split(','))
+
+    for name, param in module.named_parameters():
+        if any(layer_names):
+            if name.startswith(layer_names):
+                param.requires_grad = value
+        else:
+            param.requires_grad = value
+
+
+def tile_ctx_dict(ctx_dict, idxs):
+    """Returns dict of 3D tensors repeatedly indexed along the sample axis."""
+    # 1st: tensor, 2nd optional mask
+    return {
+        k: (tensor[:, idxs], None if mask is None else mask[:, idxs])
+        for k, (tensor, mask) in ctx_dict.items()
+    }
 
 
 def normalize_images(x):
@@ -22,29 +38,3 @@ def normalize_images(x):
     x -= mean[None, :, None, None]
     x /= std[None, :, None, None]
     return x
-
-
-def get_network_topology(direction):
-    """Parses a string in the form of 's1,s2,..,sN->t1,t2,..tN' and returns
-    a dictionary representing encoder-decoder topology."""
-    from ..utils.misc import LANGUAGES
-
-    srcs, trgs = direction.split('->')
-    srcs = srcs.split(',')
-    trgs = trgs.split(',')
-    src_languages = [s for s in srcs if s in LANGUAGES]
-    trg_languages = [s for s in trgs if s in LANGUAGES]
-    src_aux = list(set(srcs).difference(src_languages))
-    trg_aux = list(set(trgs).difference(trg_languages))
-    return {
-        'multi_src': len(srcs) > 1,
-        'multi_trg': len(trgs) > 1,
-        'src_langs': src_languages,
-        'trg_langs': trg_languages,
-        'n_src_langs': len(src_languages),
-        'n_trg_langs': len(trg_languages),
-        'src_aux': src_aux,
-        'trg_aux': trg_aux,
-        'srcs': srcs,
-        'trgs': trgs,
-    }
