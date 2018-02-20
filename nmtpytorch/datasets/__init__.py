d2e492c8a28b60345ffe1260875c45c071058446
Ozan Caglayan <ozancag@gmail.com>
2018-02-20 14:56:33 +0100

    update datasets.__init__

--- nmtpytorch/datasets/__init__.py
+++ nmtpytorch/datasets/__init__.py
@@ -1,3 +1,6 @@
 from .multi30k import Multi30kDataset
 from .bitext import BitextDataset
 from .imagefolder import ImageFolderDataset
+from .text import TextDataset
+from .multiparallel import MultiParallelDataset
+from .onehot import OneHotDataset
