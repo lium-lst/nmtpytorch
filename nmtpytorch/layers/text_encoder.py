cb04aa7cfd212f2a638c1a8cf313eecec27f07d5
Ozan Caglayan <ozancag@gmail.com>
2018-03-09 22:31:49 +0100

    text_encoder: cosmetic

--- nmtpytorch/layers/text_encoder.py
+++ nmtpytorch/layers/text_encoder.py
@@ -7,23 +7,26 @@ from ..utils.data import sort_batch
 
 class TextEncoder(nn.Module):
     """A recurrent encoder with embedding layer."""
-    def __init__(self, input_size, hidden_size, n_vocab, cell_type,
+    def __init__(self, input_size, hidden_size, n_vocab, rnn_type,
                  num_layers=1, bidirectional=True,
                  dropout_rnn=0, dropout_emb=0, dropout_ctx=0):
         super().__init__()
+
+        assert self.rnn_type in ('LSTM', 'GRU'), \
+            "rnn_type should be 'lstm' or 'gru'."
 
         self.input_size = input_size
         self.hidden_size = hidden_size
         self.ctx_size = self.hidden_size * 2
         self.n_vocab = n_vocab
-        self.cell_type = cell_type.upper()
+        self.rnn_type = rnn_type.upper()
         self.num_layers = num_layers
         self.bidirectional = bidirectional
 
         # For dropout btw layers, only effective if num_layers > 1
         self.dropout_rnn = dropout_rnn
 
-        # Our other custom dropouts after embeddings and encodings
+        # Our other custom dropouts after embeddings and annotations
         self.dropout_emb = dropout_emb
         self.dropout_ctx = dropout_ctx
 
@@ -32,20 +35,15 @@ class TextEncoder(nn.Module):
         if self.dropout_ctx > 0:
             self.do_ctx = nn.Dropout(self.dropout_ctx)
 
-        assert self.cell_type in ('LSTM', 'GRU'), \
-            "cell_type should be 'lstm' or 'gru'."
-
-        # Get the relevant class
-        rnn_cell = getattr(nn, self.cell_type)
-
         # Create embedding layer
         self.emb = nn.Embedding(self.n_vocab, self.input_size, padding_idx=0)
 
-        # Create encoder
-        self.enc = rnn_cell(self.input_size, self.hidden_size,
-                            self.num_layers, bias=True, batch_first=False,
-                            dropout=self.dropout_rnn,
-                            bidirectional=self.bidirectional)
+        # Create encoder according to the requested type
+        RNN = getattr(nn, self.rnn_type)
+        self.enc = RNN(self.input_size, self.hidden_size,
+                       self.num_layers, bias=True, batch_first=False,
+                       dropout=self.dropout_rnn,
+                       bidirectional=self.bidirectional)
 
     def forward(self, x):
         """Receives a Variable of indices (n_timesteps, n_samples) and
@@ -64,7 +62,10 @@ class TextEncoder(nn.Module):
 
         # Pack and encode
         packed_emb = pack_padded_sequence(embs, slens)
-        packed_hs, h_t = self.enc(packed_emb)
+
+        # last_state is a tuple of (h_t, c_t) for LSTM so this does indeed
+        # work transparently for both GRUs and LSTMs.
+        packed_hs, last_state = self.enc(packed_emb)
 
         # Get hidden states and revert the order
         hs = pad_packed_sequence(packed_hs)[0][:, oidxs]
