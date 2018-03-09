2d303e5e85fabe44eb75cba57d9f2e2ebc8f803a
Ozan Caglayan <ozancag@gmail.com>
2018-03-09 11:55:36 +0100

    filterchain: add de-spm for Google Sentencepiece post-processing

--- nmtpytorch/utils/filterchain.py
+++ nmtpytorch/utils/filterchain.py
@@ -6,8 +6,25 @@ from .misc import get_temp_file, fopen
 
 
 class FilterChain(object):
-    filters = {
+    """A sequential filter chain to post-process list of tokens.
+
+        Arguments:
+            filters(str): A string containing comma-separated list of filters
+                to apply.
+
+        Available Filters:
+            'de-bpe': Stitches back subword units produced with apply_bpe
+            'de-spm': Stitches back sentence pieces produced with spm_encode
+            'de-segment': Converts <tag:morpheme> to normal form
+            'de-compond': Stitches back German compound splittings
+            'c2w': Stitches back space delimited characters to words.
+                Necessary for word-level BLEU, etc. when using CharNMT.
+    """
+    FILTERS = {
         'de-bpe': lambda s: s.replace("@@ ", "").replace("@@", ""),
+        # Decoder for Google sentenpiece
+        # only for default params of spm_encode
+        'de-spm': lambda s: s.replace(" ", "").replace("\u2581", " ").strip(),
         # Converts segmentations of <tag:morpheme> to normal form
         'de-segment': lambda s: re.sub(' *<.*?:(.*?)>', '\\1', s),
         # Space delim character sequence to non-tokenized normal word form
@@ -19,11 +36,11 @@ class FilterChain(object):
         'upper': lambda s: s.upper(),
     }
 
-    def __init__(self, _filters):
-        self._filters = _filters.split(',')
-        assert not set(self._filters).difference(self.filters.keys()), \
-            "Unknown evaluation filter given in train.eval_filters"
-        self.funcs = [self.filters[k] for k in self._filters]
+    def __init__(self, filters):
+        self.filters = filters.split(',')
+        assert not set(self.FILTERS).difference(self.FILTERS.keys()), \
+            "Unknown evaluation filter given in train.evalfilters"
+        self.funcs = [self.FILTERS[k] for k in self.filters]
 
     def _apply(self, list_of_strs):
         for func in self.funcs:
@@ -31,6 +48,14 @@ class FilterChain(object):
         return list_of_strs
 
     def __call__(self, inp):
+        """Applies the filterchain on a given input.
+
+        Arguments:
+            inp(pathlib.Path or list): If a `Path` given, temporary
+                file(s) with filters applied are returned. The `Path` can
+                also be a glob expression.
+                Otherwise, a list with filtered sentences is returned.
+        """
         if isinstance(inp, Path):
             # Need to create copies of reference files with filters applied
             # and return their paths instead
@@ -51,3 +76,6 @@ class FilterChain(object):
 
         elif isinstance(inp, list):
             return self._apply(inp)
+
+    def __repr__(self):
+        return "FilterChain({})".format(" -> ".join(self.filters))
