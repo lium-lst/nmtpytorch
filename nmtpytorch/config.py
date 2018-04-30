7919d57c70637712c7bb82f65115f6a22f9b51b5
Ozan Caglayan <ozancag@gmail.com>
2018-04-30 13:59:49 +0200

    add test command, remove resume, improve seeding

--- nmtpytorch/config.py
+++ nmtpytorch/config.py
@@ -16,28 +16,32 @@ TRAIN_DEFAULTS = {
     'device_id': 'auto_1',       # auto_N for automatic N gpus
                                  # 0,1,2 for manual N gpus
                                  # 0 for 0th (single) GPU
-    'seed': 1234,                # RNG seed. 0 -> Don't init
+    'seed': 0,                   # > 0: only if you want to reproduce a previous experiment!
     'gclip': 5.,                 # Clip gradients above clip_c
     'l2_reg': 0.,                # L2 penalty factor
     'patience': 20,              # Early stopping patience
     'optimizer': 'adam',         # adadelta, sgd, rmsprop, adam
     'lr': 0.0004,                # 0 -> Use default lr from Pytorch
+    'momentum': 0.0,             # momentum for SGD
+    'nesterov': False,           # Enable Nesterov for SGD
     'disp_freq': 30,             # Training display frequency (/batch)
     'batch_size': 32,            # Training batch size
     'max_epochs': 100,           # Max number of epochs to train
     'max_iterations': int(1e6),  # Max number of updates to train
     'eval_metrics': 'loss',      # comma sep. metrics, 1st -> earlystopping
-    'eval_filters': '',          # comma sep. filters to apply to refs and hyps
+    'eval_filters': '',          # comma sep. filters to apply to refs/hyps
     'eval_beam': 6,              # Validation beam size
     'eval_batch_size': 16,       # batch_size for GPU beam-search
     'eval_freq': 3000,           # 0 means 'End of epochs'
     'eval_start': 1,             # Epoch which validation will start
     'eval_zero': False,          # Evaluate once before starting training
-                                 # Useful when using pretrained init.
+                                 # Useful when using pretrained_file
     'save_best_metrics': True,   # Save best models for each eval_metric
     'checkpoint_freq': 5000,     # Periodic checkpoint frequency
     'n_checkpoints': 5,          # Number of checkpoints to keep
     'tensorboard_dir': '',       # Enable TB and give global log folder
+    'pretrained_file': '',       # A .ckpt file from which layers will be initialized
+    'freeze_layers': '',         # comma sep. list of layer prefixes to freeze
 }
 
 
---
+++ nmtpytorch/tester.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+import time
+from pathlib import Path
+
+from .utils.misc import load_pt_file
+
+from . import logger
+from . import models
+from .config import Options
+
+
+class Tester(object):
+    """Tester for models without beam-search."""
+
+    def __init__(self, **kwargs):
+        # Setup logger
+        self.logger = logger.setup(None, 'translate')
+
+        # Store attributes directly. See bin/nmtpy for their list.
+        self.__dict__.update(kwargs)
+
+        # How many models?
+        if len(self.models) > 1:
+            raise RuntimeError("Test mode requires single model file.")
+
+        self.model_file = self.models[0]
+
+        weights, _, opts = load_pt_file(self.model_file)
+        opts = Options.from_dict(opts)
+        instance = getattr(models, opts.train['model_type'])(
+            opts=opts, logger=self.logger)
+
+        if instance.supports_beam_search:
+            self.logger.info("Model supports beam-search by the way.")
+
+        # Setup layers
+        instance.setup(is_train=False)
+        # Load weights
+        instance.load_state_dict(weights, strict=True)
+        # Move to GPU
+        instance.cuda()
+        # Switch to eval mode
+        instance.train(False)
+
+        self.instance = instance
+
+        # Can be a comma separated list of hardcoded test splits
+        if self.splits:
+            self.logger.info('Will process "{}"'.format(self.splits))
+            self.splits = self.splits.split(',')
+        elif self.source:
+            # Split into key:value's and parse into dict
+            input_dict = {}
+            self.logger.info('Will process input configuration:')
+            for data_source in self.source.split(','):
+                key, path = data_source.split(':', 1)
+                input_dict[key] = Path(path)
+                self.logger.info(' {}: {}'.format(key, input_dict[key]))
+            self.instance.opts.data['new_set'] = input_dict
+            self.splits = ['new']
+
+    def test(self, instance, split):
+        instance.load_data(split)
+        loader = instance.datasets[split].get_iterator(
+                self.batch_size, inference=True)
+
+        self.logger.info('Starting computation')
+        start = time.time()
+        results = instance.test_performance(
+            loader,
+            dump_file="{}.{}".format(self.model_file, split))
+        up_time = time.time() - start
+        self.logger.info('Took {:.3f} seconds'.format(up_time))
+        return results
+
+    def __call__(self):
+        for input_ in self.splits:
+            results = self.test(self.instance, input_)
+            for res in results:
+                print('  {}: {:.5f}'.format(res.name, res.score))
