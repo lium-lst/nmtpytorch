0b0a8c0fce3553d5a69d5f597277a6412d2001af
Ozan Caglayan <ozancag@gmail.com>
2018-04-30 12:32:48 +0200

    metrics: keep a list of beam-search metrics

--- nmtpytorch/metrics/__init__.py
+++ nmtpytorch/metrics/__init__.py
@@ -2,3 +2,5 @@ from .metric import Metric
 from .multibleu import BLEUScorer
 from .sacrebleu import SACREBLEUScorer
 from .meteor import METEORScorer
+
+beam_metrics = ["BLEU", "SACREBLEU", "METEOR"]
