cdf458feb13e87f439cc3fe8b60867bece381709
Ozan Caglayan <ozancag@gmail.com>
2018-04-30 13:58:17 +0200

    add Flatten and SimpleGRUDecoder layers

---
+++ nmtpytorch/layers/flatten.py
@@ -0,0 +1,12 @@
+import torch
+
+class Flatten(torch.nn.Module):
+    """A flatten module to squeeze single dimensions."""
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        return x.view(x.size(0), -1)
+
+    def __repr__(self):
+        return "Flatten()"
