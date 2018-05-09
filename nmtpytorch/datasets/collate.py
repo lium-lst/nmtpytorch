b95ea845ce65d0c85f78baac24e101ebb07b9820
Ozan Caglayan <ozancag@gmail.com>
2018-05-09 16:20:07 +0200

    datasets.collate: convert numpy to torch float32 tensor when batching

--- nmtpytorch/datasets/collate.py
+++ nmtpytorch/datasets/collate.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from collections import UserDict
 
+import numpy as np
 import torch
 
 from ..utils.data import pad_data, onehot_data
@@ -23,9 +24,12 @@ def get_collate(data_sources):
                 # Hack: we inject n_classes into DataSource keys
                 # from the model itself.
                 tensors[ds] = onehot_data(batch_data, ds._n_classes)
-            elif ds._type in ("ImageFolder", "Numpy"):
+            elif ds._type == "ImageFolder":
                 # Underlying data is already converted Torch tensor
                 tensors[ds] = torch.stack(batch_data)
+            elif ds._type == "Numpy":
+                tensors[ds] = torch.from_numpy(
+                    np.array(batch_data, dtype='float32'))
 
         return tensors
     return collate_fn
