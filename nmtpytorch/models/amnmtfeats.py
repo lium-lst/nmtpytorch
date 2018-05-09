3bf107df582cafb9a43aafadadf890c4f380b3eb
Ozan Caglayan <ozancag@gmail.com>
2018-05-09 16:12:22 +0200

    [new model] amnmtfeats: Attentive MNMT from pre-extracted CNN features

---
+++ nmtpytorch/models/amnmtfeats.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+import logging
+
+import torch
+
+from ..layers import TextEncoder, ConditionalMMDecoder
+from ..datasets import Multi30kDataset
+from .nmt import NMT
+
+logger = logging.getLogger('nmtpytorch')
+
+
+class AttentiveMNMTFeatures(NMT):
+    """An end-to-end sequence-to-sequence NMT model with visual attention over
+    pre-extracted convolutional features.
+    """
+    def set_defaults(self):
+        # Set parent defaults
+        super().set_defaults()
+        self.defaults.update({
+            'fusion_type': 'concat',    # Multimodal context fusion (sum|mul|concat)
+            'n_channels': 2048,         # depends on the features used
+            'alpha_c': 0.0,             # doubly stoch. attention
+        })
+
+    def __init__(self, opts):
+        super().__init__(opts)
+        if self.opts.model['alpha_c'] > 0:
+            self.aux_loss['alpha_reg'] = 0.0
+
+    def setup(self, is_train=True):
+        self.ctx_sizes['image'] = self.opts.model['n_channels']
+
+        ########################
+        # Create Textual Encoder
+        ########################
+        self.enc = TextEncoder(
+            input_size=self.opts.model['emb_dim'],
+            hidden_size=self.opts.model['enc_dim'],
+            n_vocab=self.n_src_vocab,
+            rnn_type=self.opts.model['enc_type'],
+            src_sorted_batches=True,
+            dropout_emb=self.opts.model['dropout_emb'],
+            dropout_ctx=self.opts.model['dropout_ctx'],
+            dropout_rnn=self.opts.model['dropout_enc'],
+            num_layers=self.opts.model['n_encoders'],
+            emb_maxnorm=self.opts.model['emb_maxnorm'],
+            emb_gradscale=self.opts.model['emb_gradscale'])
+
+        # Create Decoder
+        self.dec = ConditionalMMDecoder(
+            input_size=self.opts.model['emb_dim'],
+            hidden_size=self.opts.model['dec_dim'],
+            n_vocab=self.n_trg_vocab,
+            rnn_type=self.opts.model['dec_type'],
+            ctx_size_dict=self.ctx_sizes,
+            ctx_name=str(self.sl),
+            fusion_type=self.opts.model['fusion_type'],
+            tied_emb=self.opts.model['tied_emb'],
+            dec_init=self.opts.model['dec_init'],
+            att_type=self.opts.model['att_type'],
+            att_activ=self.opts.model['att_activ'],
+            transform_ctx=self.opts.model['att_transform_ctx'],
+            mlp_bias=self.opts.model['att_mlp_bias'],
+            att_bottleneck=self.opts.model['att_bottleneck'],
+            dropout_out=self.opts.model['dropout_out'],
+            emb_maxnorm=self.opts.model['emb_maxnorm'],
+            emb_gradscale=self.opts.model['emb_gradscale'])
+
+        # Share encoder and decoder weights
+        if self.opts.model['tied_emb'] == '3way':
+            self.enc.emb.weight = self.dec.emb.weight
+
+    def load_data(self, split):
+        """Loads the requested dataset split."""
+        self.datasets[split] = Multi30kDataset(
+            data_dict=self.opts.data[split + '_set'],
+            vocabs=self.vocabs,
+            topology=self.topology)
+        logger.info(self.datasets[split])
+
+    def encode(self, batch):
+        # Get features into (n,c,w*h) and then (w*h,n,c)
+        feats = batch['image'].view(
+            (*batch['image'].shape[:2], -1)).permute(2, 0, 1)
+
+        return {
+            'image': (feats, None),
+            str(self.sl): self.enc(batch[self.sl]),
+        }
+
+    def forward(self, batch):
+        result = super().forward(batch)
+
+        if self.training and self.opts.model['alpha_c'] > 0:
+            alpha_loss = (1 - torch.cat(self.dec.alphas).sum(0)).pow(2).sum(0)
+            self.aux_loss['alpha_reg'] = alpha_loss.mean().mul(
+                self.opts.model['alpha_c'])
+
+        return result
