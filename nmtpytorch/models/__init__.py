205924ac1e560930668e16d3eb79cfab2c3d1ab0
Ozan Caglayan <ozancag@gmail.com>
2018-05-23 12:46:12 +0200

    models: add new NMTv2

--- nmtpytorch/models/__init__.py
+++ nmtpytorch/models/__init__.py
@@ -1,5 +1,6 @@
 from .sat import ShowAttendAndTell
 from .nmt import NMT
+from .nmtv2 import NMTv2
 from .amnmt import AttentiveMNMT
 from .amnmtfeats import AttentiveMNMTFeatures
 from .mnmtdecinit import MNMTDecinit
