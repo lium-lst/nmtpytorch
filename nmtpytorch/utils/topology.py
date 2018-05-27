87b7eb096f7928026ec986ff4c7bd1a131e9d49a
Ozan Caglayan <ozancag@gmail.com>
2018-05-27 23:29:32 +0200

    topology: add to_torch() shortcut

--- nmtpytorch/utils/topology.py
+++ nmtpytorch/utils/topology.py
@@ -1,14 +1,24 @@
 # -*- coding: utf-8 -*-
 from collections import UserString, OrderedDict
 
+from .. import datasets
+
 
 class DataSource(UserString):
-    def __init__(self, name, type_):
+    def __init__(self, name, _type, src=False, trg=False):
         super().__init__(name)
-        self._type = type_
+        self._type = _type
+        self.src = src
+        self.trg = trg
+
+        # Assign the method that knows how to create a tensor for a batch
+        # of this type
+        klass = getattr(datasets, '{}Dataset'.format(_type))
+        self.to_torch = klass.to_torch
 
     def __repr__(self):
-        return "DataSource(name={}, type={})".format(self.data, self._type)
+        return "DataSource(name={}, type={}, src={}, trg={})".format(
+            self.data, self._type, self.src, self.trg)
 
 
 class Topology(object):
@@ -48,7 +58,8 @@ class Topology(object):
             for val in values:
                 name, *ftype = val.strip().split(':')
                 ftype = ftype[0] if len(ftype) > 0 else "Text"
-                ds = DataSource(name, ftype)
+                ds = DataSource(name, ftype,
+                                src=(key == 'srcs'), trg=(key == 'trgs'))
                 if name in self.all:
                     raise RuntimeError(
                         '"{}" already given as a data source.'.format(name))
@@ -62,6 +73,9 @@ class Topology(object):
     def get_trg_langs(self):
         langs = [v for v in self.trgs.values() if v._type == 'Text']
         return langs
+
+    def __getitem__(self, key):
+        return self.all[key]
 
     def __repr__(self):
         s = "Sources:\n"
