e49867a6127601d9981087833804d502fbb7d18a
Ozan Caglayan <ozancag@gmail.com>
2018-05-28 21:46:20 +0200

    remove multiparallel wrapper

--- nmtpytorch/datasets/__init__.py
+++ nmtpytorch/datasets/__init__.py
@@ -2,7 +2,6 @@ from .numpy import NumpyDataset
 from .bitext import BitextDataset
 from .imagefolder import ImageFolderDataset
 from .text import TextDataset
-from .multiparallel import MultiParallelDataset
 from .onehot import OneHotDataset
 from .multimodal import MultimodalDataset
 from .multilabelmulti30k import MultiLabelMulti30kDataset
