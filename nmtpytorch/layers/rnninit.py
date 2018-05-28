3ece719af7a7d35234d79d9c6f30d1e5bd1daf10
Ozan Caglayan <ozancag@gmail.com>
2018-05-28 23:41:56 +0200

    rnninit: fix shape

--- nmtpytorch/layers/rnninit.py
+++ nmtpytorch/layers/rnninit.py
@@ -58,8 +58,7 @@ class RNNInitializer(nn.Module):
 
     def _init_zero(self, ctx, mask):
         # h_0: (n_layers, batch_size, hidden_size)
-        h_0 = torch.zeros(
-            self.n_layers, ctx.shape[1], self.hidden_size)
+        h_0 = torch.zeros(ctx.shape[1], self.hidden_size * self.n_layers)
         return Variable(h_0).cuda()
 
     def _init_feats(self, ctx, mask):
