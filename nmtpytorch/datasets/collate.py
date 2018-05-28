a98758ae01f4a30d273810b120ce03b86a6f4412
Ozan Caglayan <ozancag@gmail.com>
2018-05-28 10:22:00 +0200

    collate: simplify, return a Batch() object

--- nmtpytorch/datasets/collate.py
+++ nmtpytorch/datasets/collate.py
@@ -1,5 +1,33 @@
-# -*- coding: utf-8 -*-
-from collections import UserDict
+from torch.autograd import Variable
+
+
+class Batch(object):
+    def __init__(self, data_dict, batch_size):
+        self.data = data_dict
+        self.keys = list(self.data.keys())
+        self.size = batch_size
+        self.device = None
+
+    def to_gpu(self, volatile=False):
+        if self.device is None:
+            self.device = 'gpu'
+            self.data = {
+                k: Variable(v, volatile=volatile).cuda() for k, v in self.data.items()}
+
+    def to_cpu(self, volatile=False):
+        if self.device is None:
+            self.device = 'cpu'
+            self.data = {
+                k: Variable(v, volatile=volatile) for k, v in self.data.items()}
+
+    def __getitem__(self, key):
+        return self.data[key]
+
+    def __repr__(self):
+        s = "Batch(size={}, device={})\n".format(self.size, self.device)
+        for key in self.keys:
+            s += "  {:10s} -> {}\n".format(str(key), self.data[key].shape)
+        return s
 
 
 def get_collate(data_sources):
@@ -7,13 +35,13 @@ def get_collate(data_sources):
     in terms of the given DataSource keys."""
 
     def collate_fn(batch):
-        tensors = UserDict()
-        tensors.size = len(batch)
+        batch_dict = {}
 
         # Iterate over keys which are DataSource objects
         for ds in data_sources:
             batch_data = [elem[ds] for elem in batch]
-            tensors[ds] = ds.to_torch(batch_data)
+            batch_dict[ds] = ds.to_torch(batch_data)
 
-        return tensors
+        return Batch(batch_dict, len(batch))
+
     return collate_fn
