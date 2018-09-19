76ed269a4d2ab551f5c48f9691087a69addf0e40
Ozan Caglayan <ozancag@gmail.com>
2018-09-19 17:45:07 +0200

    utils/nn: add mean_pool() (from: @klmulligan)

--- nmtpytorch/utils/nn.py
+++ nmtpytorch/utils/nn.py
@@ -7,6 +7,25 @@ def get_rnn_hidden_state(h):
     return h if not isinstance(h, tuple) else h[0]
 
 
+def mean_pool(data):
+    """Simple mean pool function for transforming 3D features of shape
+    [T]imesteps x [B]atch_size x [F]eature_size into 2D BxF features.
+    (author: @klmulligan)
+
+        Arguments:
+            data (tuple): Encoder result of form (data: Tensor(TxBxF), mask: Tensor(TxB))
+        Returns:
+            pooled_data (Tensor): Mean pooled data of shape BxF.
+    """
+    # Unpack
+    x, mask = data
+
+    if mask is not None:
+        return x.sum(0) / mask.sum(0).unsqueeze(1)
+    else:
+        return x.mean(0)
+
+
 # Taken from:
 # https://github.com/pytorch/pytorch/pull/5297/files
 class ModuleDict(Module):
