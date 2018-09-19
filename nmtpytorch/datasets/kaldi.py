bb8b5ccc0d36fe13cb6644c12bfb7f982a07af2f
Ozan Caglayan <ozancag@gmail.com>
2018-09-19 17:32:13 +0200

    Add Kaldi support
    Code contributed by @ramonsanabria

---
+++ nmtpytorch/datasets/kaldi.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+from pathlib import Path
+from tqdm import tqdm
+
+import numpy as np
+import torch
+from torch.utils.data import Dataset
+
+from ..utils.kaldi import readMatrixShape, readMatrixByOffset
+
+# TODO
+# ----
+# an lru_cache() decorated version of readMatrixByOffset() will make sure that
+# all the training data is cached into memory after 1 epoch.
+
+
+class KaldiDataset(Dataset):
+    """A PyTorch dataset for Kaldi .scp/ark.
+
+    Arguments:
+        fname (str or Path): A string or ``pathlib.Path`` object for
+            a folder that contains ``feats_local.scp`` and optionally a ``segments.len``
+            file containing segment lengths.
+    """
+
+    def __init__(self, fname):
+        self.data = []
+        self.lengths = []
+        self.root = Path(fname)
+        self.scp_path = self.root / 'feats_local.scp'
+        self.len_path = self.root / 'segments.len'
+
+        if not self.scp_path.exists():
+            raise RuntimeError('{} does not exist.'.format(self.scp_path))
+
+        if self.len_path.exists():
+            read_lengths = False
+            # Read lengths file
+            with open(self.len_path) as f:
+                for line in f:
+                    self.lengths.append(int(line.strip()))
+        else:
+            # Read them below (this is slow)
+            read_lengths = True
+
+        with open(self.scp_path) as scp_input_file:
+            for line in tqdm(scp_input_file, unit='segments'):
+                uttid, pointer = line.strip().split()
+                arkfile, offset = pointer.rsplit(':', 1)
+                offset = int(offset)
+                self.data.append((arkfile, offset))
+                if read_lengths:
+                    with open(arkfile, "rb") as g:
+                        g.seek(offset)
+                        feat_len = readMatrixShape(g)[0]
+
+                    self.lengths.append(feat_len)
+
+        # Set dataset size
+        self.size = len(self.data)
+
+        if self.size != len(self.lengths):
+            raise RuntimeError("Dataset size and lengths size does not match.")
+
+    @staticmethod
+    def to_torch(batch):
+        # Move batch dimension to middle
+        return torch.from_numpy(np.array(batch, dtype='float32')).transpose(0, 1)
+
+    def __getitem__(self, idx):
+        """Read segment features from the actual .ark file."""
+        return readMatrixByOffset(*self.data[idx])
+
+    def __len__(self):
+        return self.size
+
+    def __repr__(self):
+        s = "{} '{}' ({} samples)\n".format(
+            self.__class__.__name__, self.scp_path.name, self.__len__())
+        return s
---
+++ nmtpytorch/utils/kaldi.py
@@ -0,0 +1,196 @@
+import os
+import numpy
+import struct
+import functools
+
+ERROR_BINARY = "Binary mode header ('\0B') not found when reading a matrix."
+ERROR_READ_MAT = "Unknown matrix format '{}'. Supported ones: DM(float64), FM(float32)."
+ERROR_WRITE_MAT = "Unknown matrix format '{}'. Supported ones are float64, float32."
+
+
+def readString(f):
+    s = ""
+    while True:
+        c = f.read(1).decode('utf-8')
+        if c == "":
+            raise ValueError("EOF encountered while reading a string.")
+        if c == " ":
+            return s
+        s += c
+
+
+def readInteger(f):
+    n = ord(f.read(1))
+    a = f.read(n)[::-1]
+    try:
+        return int.from_bytes(a, byteorder='big', signed=False)
+    except Exception as e:
+        return functools.reduce(lambda x, y: x * 256 + ord(y), a, 0)
+
+
+def readMatrix(f):
+    header = f.read(2).decode('utf-8')
+    if header != "\0B":
+        raise ValueError(ERROR_BINARY)
+    mat_format = readString(f)
+    nRows = readInteger(f)
+    nCols = readInteger(f)
+    if mat_format == "DM":
+        data = struct.unpack("<%dd" % (nRows * nCols), f.read(nRows * nCols * 8))
+        data = numpy.array(data, dtype="float64")
+    elif mat_format == "FM":
+        data = struct.unpack("<%df" % (nRows * nCols), f.read(nRows * nCols * 4))
+        data = numpy.array(data, dtype="float32")
+    else:
+        raise ValueError(ERROR_READ_MAT.format(mat_format))
+    return data.reshape(nRows, nCols)
+
+
+def readMatrixShape(f):
+    header = f.read(2).decode('utf-8')
+    if header != "\0B":
+        raise ValueError(
+            "Binary mode header ('\0B') not found when attempting to read a matrix.")
+    mat_format = readString(f)
+    nRows = readInteger(f)
+    nCols = readInteger(f)
+    if mat_format == "DM":
+        f.seek(nRows * nCols * 8, os.SEEK_CUR)
+    elif mat_format == "FM":
+        f.seek(nRows * nCols * 4, os.SEEK_CUR)
+    else:
+        raise ValueError(ERROR_READ_MAT.format(mat_format))
+    return nRows, nCols
+
+
+def writeString(f, s):
+    f.write((s + " ").encode('utf-8'))
+
+
+def writeInteger(f, a):
+    s = struct.pack("<i", a)
+    f.write(chr(len(s)).encode('utf-8') + s)
+
+
+def writeMatrix(f, data):
+    f.write('\0B'.encode('utf-8'))      # Binary data header
+    if str(data.dtype) == "float64":
+        writeString(f, "DM")
+        writeInteger(f, data.shape[0])
+        writeInteger(f, data.shape[1])
+        f.write(struct.pack("<%dd" % data.size, *data.ravel()))
+    elif str(data.dtype) == "float32":
+        writeString(f, "FM")
+        writeInteger(f, data.shape[0])
+        writeInteger(f, data.shape[1])
+        f.write(struct.pack("<%df" % data.size, *data.ravel()))
+    else:
+        raise ValueError(ERROR_WRITE_MAT.format(str(data.dtype)))
+
+
+def readArk(filename, limit=numpy.inf):
+    """
+    Reads the features in a Kaldi ark file.
+    Returns a list of feature matrices and a list of the utterance IDs.
+    """
+    features = []
+    uttids = []
+    with open(filename, "rb") as f:
+        while True:
+            try:
+                uttid = readString(f)
+            except ValueError:
+                break
+            feature = readMatrix(f)
+            features.append(feature)
+            uttids.append(uttid)
+            if len(features) == limit:
+                break
+    return features, uttids
+
+
+def readMatrixByOffset(arkfile, offset):
+    with open(arkfile, "rb") as g:
+        g.seek(offset)
+        feature = readMatrix(g)
+    return feature
+
+
+def readScp(filename, limit=numpy.inf):
+    """
+    Reads the features in a Kaldi script file.
+    Returns a list of feature matrices and a list of the utterance IDs.
+    """
+    features = []
+    uttids = []
+    with open(filename, "r") as f:
+        for line in f:
+            uttid, pointer = line.strip().split()
+            p = pointer.rfind(":")
+            arkfile, offset = pointer[:p], int(pointer[p + 1:])
+            with open(arkfile, "rb") as g:
+                g.seek(offset)
+                feature = readMatrix(g)
+            features.append(feature)
+            uttids.append(uttid)
+            if len(features) == limit:
+                break
+    return features, uttids
+
+
+def read_scp_info(filename, limit=numpy.inf):
+    res = []
+    with open(filename, "r") as f:
+        for line in f:
+            uttid, pointer = line.strip().split()
+            p = pointer.rfind(":")
+            arkfile, offset = pointer[:p], int(pointer[p + 1:])
+            with open(arkfile, "rb") as g:
+                g.seek(offset)
+                feat_len, feat_dim = readMatrixShape(g)
+            res.append((uttid, arkfile, offset, feat_len, feat_dim))
+            if len(res) == limit:
+                break
+    return res
+
+
+def read_scp_info_dic(filename, limit=numpy.inf):
+    res = {}
+    with open(filename, "r") as f:
+        for line in f:
+            uttid, pointer = line.strip().split()
+            p = pointer.rfind(":")
+            arkfile, offset = pointer[:p], int(pointer[p + 1:])
+            with open(arkfile, "rb") as g:
+                g.seek(offset)
+                feat_len, feat_dim = readMatrixShape(g)
+            res[uttid] = ((uttid, arkfile, offset, feat_len, feat_dim))
+            if len(res) == limit:
+                break
+    return res
+
+
+def writeArk(filename, features, uttids):
+    """
+    Takes a list of feature matrices and a list of utterance IDs,
+      and writes them to a Kaldi ark file.
+    Returns a list of strings in the format "filename:offset",
+      which can be used to write a Kaldi script file.
+    """
+    pointers = []
+    with open(filename, "ab") as f:
+        for feature, uttid in zip(features, uttids):
+            writeString(f, uttid)
+            pointers.append("%s:%d" % (filename, f.tell()))
+            writeMatrix(f, feature)
+    return pointers
+
+
+def writeScp(filename, uttids, pointers):
+    """
+    Takes a list of utterance IDs and a list of strings in the format "filename:offset",
+      and writes them to a Kaldi script file.
+    """
+    with open(filename, "w") as f:
+        for uttid, pointer in zip(uttids, pointers):
+            f.write("%s %s\n" % (uttid, pointer))
