b995dc6fc75d2d871926d1335772f69585169ad9
Ozan Caglayan <ozancag@gmail.com>
2018-09-19 17:37:06 +0200

    optimizer: add LR decaying

--- nmtpytorch/optimizer.py
+++ nmtpytorch/optimizer.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
+import logging
+
 import torch.optim
+from torch.optim.lr_scheduler import ReduceLROnPlateau
 from torch.nn.utils.clip_grad import clip_grad_norm
+
+logger = logging.getLogger('nmtpytorch')
 
 # Setup optimizer (should always come after model.cuda())
 # iterable of dicts for per-param options where each dict
@@ -31,10 +36,17 @@ class Optimizer(object):
             filter(lambda p: p[1].requires_grad, model.named_parameters()))
 
     def __init__(self, name, model, lr=0, momentum=0.0,
-                 nesterov=False, weight_decay=0, gclip=0):
+                 nesterov=False, weight_decay=0, gclip=0,
+                 lr_decay=False, lr_decay_factor=0.1, lr_decay_mode='min',
+                 lr_decay_patience=10, lr_decay_min=0.000001):
         self.name = name
         self.model = model
-        self.lr = lr
+        self.initial_lr = lr
+        self.lr_decay = lr_decay
+        self.lr_decay_factor = lr_decay_factor
+        self.lr_decay_mode = lr_decay_mode
+        self.lr_decay_patience = lr_decay_patience
+        self.lr_decay_min = lr_decay_min
         self.momentum = momentum
         self.nesterov = nesterov
         self.weight_decay = weight_decay
@@ -42,8 +54,8 @@ class Optimizer(object):
 
         self.optim_args = {}
         # If an explicit lr given, pass it to torch optimizer
-        if self.lr > 0:
-            self.optim_args['lr'] = self.lr
+        if self.initial_lr > 0:
+            self.optim_args['lr'] = self.initial_lr
 
         if self.name == 'sgd':
             self.optim_args['momentum'] = self.momentum
@@ -78,7 +90,8 @@ class Optimizer(object):
         self.optim = self.methods[self.name](self.param_groups, **self.optim_args)
 
         # Get final lr that will be used
-        self.lr = self.optim.defaults['lr']
+        self.initial_lr = self.optim.defaults['lr']
+        self.cur_lr = self.initial_lr
 
         # Assign shortcuts
         self.zero_grad = self.optim.zero_grad
@@ -88,16 +101,42 @@ class Optimizer(object):
         else:
             self.step = self._step
 
+        if self.lr_decay:
+            self.scheduler = ReduceLROnPlateau(
+                self.optim, mode=self.lr_decay_mode,
+                factor=self.lr_decay_factor, patience=self.lr_decay_patience,
+                min_lr=self.lr_decay_min)
+        else:
+            self.scheduler = None
+
     def _step(self, closure=None):
         """Gradient clipping aware step()."""
         clip_grad_norm(self.params, self.gclip)
         self.optim.step(closure)
 
+    def lr_step(self, metric):
+        if self.scheduler is not None:
+            self.scheduler.step(metric)
+            if self.get_lr() != self.cur_lr:
+                self.cur_lr = self.get_lr()
+                logger.info('** Learning rate changed -> {}'.format(self.cur_lr))
+                # Signal it back
+                return True
+        else:
+            return False
+
+    def get_lr(self):
+        """Returns current lr for parameters."""
+        return self.optim.param_groups[0]['lr']
+
     def __repr__(self):
         s = "Optimizer => {} (lr: {}, weight_decay: {}, g_clip: {}".format(
-            self.name, self.lr, self.weight_decay, self.gclip)
+            self.name, self.initial_lr, self.weight_decay, self.gclip)
         if self.name == 'sgd':
             s += ', momentum: {}, nesterov: {}'.format(
                 self.momentum, self.nesterov)
+        if self.lr_decay:
+            s += ', lr_decay: (patience={}, factor={})'.format(
+                self.lr_decay_patience, self.lr_decay_factor)
         s += ')'
         return s
