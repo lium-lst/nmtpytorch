c0f94efa98f1932ac7ee02ec463c2978d7f945bd
Ozan Caglayan <ozancag@gmail.com>
2018-09-24 23:19:51 +0200

    setup.py: update

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import shutil
 import pathlib
-from setuptools import setup
+import setuptools
 
 
 def get_nmtpytorch_version():
@@ -12,29 +12,26 @@ def get_nmtpytorch_version():
         return eval(s.split(' ')[-1])
 
 
-# Set up packages to install
-packages = ['nmtpytorch']
-packages.extend(['nmtpytorch.%s' % p for p in
-                ('datasets', 'layers', 'metrics', 'models',
-                 'samplers', 'utils')])
-packages.extend(['nmtpytorch.cocoeval.%s' % m for m in
-                 ('bleu', 'meteor', 'cider', 'rouge')])
-
 if shutil.which('java') is None:
     print("*** WARNING: 'java' not found.")
     print("*** WARNING: You need to have JRE installed for METEOR to work.")
 
 
-setup(
+with open('README.md') as f:
+    long_description = f.read()
+
+
+setuptools.setup(
     name='nmtpytorch',
     version=get_nmtpytorch_version(),
     description='Neural Machine Translation Framework in PyTorch',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/lium-lst/nmtpytorch',
     author='Ozan Caglayan, LIUM LST Team',
     author_email='ozancag@gmail.com',
     license='MIT',
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Science/Research',
         'Topic :: Scientific/Engineering',
         'License :: OSI Approved :: MIT License',
@@ -42,15 +39,15 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Operating System :: POSIX',
     ],
-    keywords='nmt neural-mt translation deep-learning pytorch captioning',
+    keywords='nmt neural-mt translation sequence-to-sequence deep-learning pytorch',
     install_requires=[
         'numpy', 'scipy', 'scikit-learn', 'tqdm', 'pillow',
-        'torch==0.3.1', 'torchvision==0.2.1',
+        'torch', 'torchvision==0.2.1',
         'sacrebleu>=1.2.9', 'tensorboardX==1.1',
         'editdistance==0.4', 'ipdb', 'subword_nmt==0.3.5',
     ],
     include_package_data=True,
     exclude_package_data={'': ['.git']},
-    packages=packages,
+    packages=setuptools.find_packages(),
     scripts=[str(p) for p in pathlib.Path('bin').glob('*')],
     zip_safe=False)
