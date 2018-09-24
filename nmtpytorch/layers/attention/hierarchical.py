927500324a304763a373fd6d2391db7dd496479e
Ozan Caglayan <ozancag@gmail.com>
2018-09-24 12:38:27 +0200

    hierarchical attention (@jlibovicky)

---
+++ nmtpytorch/layers/attention/hierarchical.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+# Libovický, J., & Helcl, J. (2017). Attention Strategies for Multi-Source
+# Sequence-to-Sequence Learning. In Proceedings of the 55th Annual Meeting of
+# the Association for Computational Linguistics (Volume 2: Short Papers)
+# (Vol. 2, pp. 196-202). [Code contributed by @jlibovicky]
+
+
+class HierarchicalAttention(nn.Module):
+    """Hierarchical attention over multiple modalities."""
+    def __init__(self, ctx_dims, hid_dim, mid_dim, att_activ='tanh'):
+        super().__init__()
+
+        self.activ = getattr(F, att_activ)
+        self.ctx_dims = ctx_dims
+        self.hid_dim = hid_dim
+        self.mid_dim = mid_dim
+
+        self.ctx_projs = nn.ModuleList([
+            nn.Linear(dim, mid_dim, bias=False) for dim in self.ctx_dims])
+        self.dec_proj = nn.Linear(hid_dim, mid_dim, bias=True)
+        self.mlp = nn.Linear(self.mid_dim, 1, bias=False)
+
+    def forward(self, contexts, hid):
+        dec_state_proj = self.dec_proj(hid)
+        ctx_projected = torch.cat([
+            p(ctx).unsqueeze(0) for p, ctx
+            in zip(self.ctx_projs, contexts)], dim=0)
+        energies = self.mlp(self.activ(dec_state_proj + ctx_projected))
+        att_dist = F.softmax(energies, dim=0)
+
+        ctxs_cat = torch.cat([c.unsqueeze(0) for c in contexts])
+        joint_context = (att_dist * ctxs_cat).sum(0)
+
+        return att_dist, joint_context
