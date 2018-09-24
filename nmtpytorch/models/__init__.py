fa6bde8083852fb1d4a23c81bffc742991f892f3
Ozan Caglayan <ozancag@gmail.com>
2018-09-24 23:02:00 +0200

    models: add ASRv2 alias for ASR

--- nmtpytorch/models/__init__.py
+++ nmtpytorch/models/__init__.py
@@ -1,6 +1,25 @@
 from .sat import ShowAttendAndTell
 from .nmt import NMT
-from .nmtv2 import NMTv2
 from .amnmt import AttentiveMNMT
 from .amnmtfeats import AttentiveMNMTFeatures
 from .mnmtdecinit import MNMTDecinit
+
+# Speech models
+from .asr import ASR
+from .mmasr import MultimodalASR
+from .mm_action_asr import MMActionASR
+
+# Multi-task speech models
+from .asr_nmt import ASRNMT
+from .asr_nmt_shared import ASRNMTShared
+
+# Multi-task SLT models
+from .slt_asr_otm import SLTASROneToMany
+
+from .multitask import Multitask
+from .multitask_att import MultitaskAtt
+
+##########################################
+# Backward-compatibility with older models
+##########################################
+ASRv2 = ASR
