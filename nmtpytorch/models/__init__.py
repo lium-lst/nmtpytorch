2b1beeca1b5bc604801a0282777ff37fbab7eadc
Ozan Caglayan <ozancag@gmail.com>
2018-09-25 13:03:48 +0200

    models: update __init__

--- nmtpytorch/models/__init__.py
+++ nmtpytorch/models/__init__.py
@@ -6,18 +6,10 @@ from .mnmtdecinit import MNMTDecinit
 
 # Speech models
 from .asr import ASR
-from .mmasr import MultimodalASR
-from .mm_action_asr import MMActionASR
 
-# Multi-task speech models
-from .asr_nmt import ASRNMT
-from .asr_nmt_shared import ASRNMTShared
-
-# Multi-task SLT models
-from .slt_asr_otm import SLTASROneToMany
-
-from .multitask import Multitask
-from .multitask_att import MultitaskAtt
+# Requires work
+#from .multitask import Multitask
+#from .multitask_att import MultitaskAtt
 
 ##########################################
 # Backward-compatibility with older models
