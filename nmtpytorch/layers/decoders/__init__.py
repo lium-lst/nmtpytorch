a4b09bf95ec114521e57a50f41f979809d1c2cd0
Ozan Caglayan <ozancag@gmail.com>
2018-11-14 19:16:29 +0100

    decoders.__init__: add decoder getter

--- nmtpytorch/layers/decoders/__init__.py
+++ nmtpytorch/layers/decoders/__init__.py
@@ -1,7 +1,17 @@
-from .simplegru import SimpleGRUDecoder
 from .conditional import ConditionalDecoder
+from .simplegru import SimpleGRUDecoder
 from .conditionalmm import ConditionalMMDecoder
 from .multisourceconditional import MultiSourceConditionalDecoder
 from .xu import XuDecoder
 from .switchinggru import SwitchingGRUDecoder
 from .vector import VectorDecoder
+
+
+def get_decoder(type_):
+    """Only expose ones with compatible __init__() arguments for now."""
+    return {
+        'cond': ConditionalDecoder,
+        'simplegru': SimpleGRUDecoder,
+        'condmm': ConditionalMMDecoder,
+        'vector': VectorDecoder,
+    }[type_]
