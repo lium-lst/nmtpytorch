1f93aced3883191ab62afa8e04d55d59601e8e8f
Ozan Caglayan <ozancag@gmail.com>
2018-11-14 19:31:13 +0100

    MultimodalDataset: pass **kwargs to datasets

--- nmtpytorch/datasets/multimodal.py
+++ nmtpytorch/datasets/multimodal.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 import logging
 from torch.utils.data import Dataset
-from torch.utils.data.sampler import BatchSampler, SequentialSampler
+from torch.utils.data.sampler import BatchSampler, SequentialSampler, RandomSampler
 
-from . import (ImageFolderDataset, TextDataset, OneHotDataset, NumpyDataset,
-               KaldiDataset, NumpySequenceDataset, ShelveDataset)
+from . import get_dataset
 from .collate import get_collate
 from ..samplers import BucketBatchSampler, ApproximateBucketBatchSampler
 
@@ -21,17 +20,20 @@ class MultimodalDataset(Dataset):
         vocabs(dict): dictionary mapping keys to Vocabulary() objects
         topology(Topology): A topology object.
         bucket_by(str): String identifier of the modality which will define how
-            the batches will be bucketed, i.e. sort key.
+            the batches will be bucketed, i.e. sort key. If `None`, no
+            bucketing will be performed but the layers and models should
+            support packing/padding/masking for this to work.
         max_len(int, optional): Maximum sequence length for ``bucket_by``
-            modality to reject batches with long sequences.
+            modality to reject batches with long sequences. Does not have an effect
+            if mode != 'train'.
         bucket_order (str, optional): ``ascending`` or ``descending`` to
             perform length-based curriculum learning. Default is ``None``
-            which shuffles bucket order.
+            which shuffles bucket order. Does not have an effect if mode != 'train'.
         sampler_type(str, optional): 'bucket' or 'approximate' (Default: 'bucket')
-        kwargs (dict): Argument dictionary for datasets.
+        kwargs (dict): Additional arguments to pass to the dataset constructors.
     """
     def __init__(self, data, mode, batch_size, vocabs, topology,
-                 bucket_by, max_len=None, bucket_order=None,
+                 bucket_by, bucket_order=None, max_len=None,
                  sampler_type='bucket', **kwargs):
         self.datasets = {}
         self.mode = mode
@@ -39,81 +41,81 @@ class MultimodalDataset(Dataset):
         self.batch_size = batch_size
         self.topology = topology
         self.bucket_by = bucket_by
-        if sampler_type == 'approximate':
-            gen_sampler = ApproximateBucketBatchSampler
-        elif sampler_type == 'bucket':
-            gen_sampler = BucketBatchSampler
+        self.sampler_type = sampler_type
 
         # Disable filtering if not training
         self.max_len = max_len if self.mode == 'train' else None
+
+        # This is only useful for training
         self.bucket_order = bucket_order if self.mode == 'train' else None
 
         # For old models to work, set it to the first source
         if self.bucket_by is None:
-            if len(self.topology.get_src_langs()) > 0:
-                self.bucket_by = self.topology.get_src_langs()[0]
-            elif self.mode != 'beam' and len(self.topology.get_trg_langs()) > 0:
-                self.bucket_by = self.topology.get_trg_langs()[0]
+            logger.info(
+                'WARNING: Bucketing sampler disabled. It is up to the model '
+                'to take care of packing/padding/masking if any.')
 
-        # TODO: This should be agnostic to datasets, i.e. no explicit calls
-        # to dataset constructors should be necessary.
         for key, ds in self.topology.all.items():
             if self.mode == 'beam' and ds.trg:
-                # Skip target streams
+                # Skip target streams for beam-search
                 continue
 
-            if key == self.bucket_by:
-                self.bucket_by = ds
+            try:
+                # Get the relevant dataset class
+                dataset_constructor = get_dataset(ds._type)
+            except KeyError as ke:
+                logger.info("ERROR: Unknown dataset type '{}'".format(ds._type))
 
-            if ds._type == "Text":
-                # Prepend <bos> if datasource is on target side
-                self.datasets[ds] = TextDataset(
-                    data[key], vocabs[key], bos=ds.trg)
-            elif ds._type == "OneHot":
-                self.datasets[ds] = OneHotDataset(data[key], vocabs[key])
-            elif ds._type == "ImageFolder":
-                self.datasets[ds] = ImageFolderDataset(data[key], **kwargs)
-            elif ds._type == "Numpy":
-                self.datasets[ds] = NumpyDataset(data[key])
-            elif ds._type == "Shelve":
-                self.datasets[ds] = ShelveDataset(data[key], **kwargs)
-            elif ds._type == "Kaldi":
-                self.datasets[ds] = KaldiDataset(data[key])
-            elif ds._type == "NumpySequence":
-                self.datasets[ds] = NumpySequenceDataset(data[key], **kwargs)
-            else:
-                raise ValueError("Unknown dataset type: {}.".format(ds))
+            # Construct the dataset
+            logger.info("Initializing dataset for '{}'".format(ds))
+            self.datasets[ds] = dataset_constructor(
+                fname=data[key],
+                vocab=vocabs.get(key, None), bos=ds.trg, **kwargs)
 
         # Detect dataset sizes
-        sizes = set()
-        for dataset in self.datasets.values():
-            sizes.add(len(dataset))
+        sizes = set([len(dataset) for dataset in self.datasets.values()])
         assert len(sizes) == 1, "Non-parallel datasets are not supported."
-
-        # Set dataset size
-        self.size = list(sizes)[0]
 
         # Set list of available datasets
         self.keys = list(self.datasets.keys())
 
-        self.n_sources = len([k for k in self.keys if k.src])
-        self.n_targets = len([k for k in self.keys if k.trg])
-
+        # Get collator
         self.collate_fn = get_collate(self.keys)
-        if self.bucket_by is not None:
+
+        if self.bucket_by is not None and self.bucket_by in self.datasets:
+            if self.sampler_type == 'approximate':
+                gen_sampler = ApproximateBucketBatchSampler
+            elif self.sampler_type == 'bucket':
+                gen_sampler = BucketBatchSampler
             self.sort_lens = self.datasets[self.bucket_by].lengths
             self.sampler = gen_sampler(
                 batch_size=self.batch_size,
                 sort_lens=self.sort_lens,
                 max_len=self.max_len,
-                store_indices=self.mode == 'beam',
+                store_indices=self.mode != 'train',
                 order=self.bucket_order)
         else:
-            # No modality to sort batches, return sequential data
+            # bucket_by only valid for training
+            if self.bucket_by:
+                self.bucket_by = None
+                logger.info('Disabling bucketing for data loader.')
+            # No modality provided to bucket sequential batches
             # Used for beam-search in image->text tasks
+            if self.mode == 'beam':
+                sampler = SequentialSampler(self)
+                self.sampler_type = 'sequential'
+            else:
+                sampler = RandomSampler(self)
+                self.sampler_type = 'random'
             self.sampler = BatchSampler(
-                SequentialSampler(self),
-                batch_size=self.batch_size, drop_last=False)
+                sampler, batch_size=self.batch_size, drop_last=False)
+
+        # Set some metadata
+        self.n_sources = len([k for k in self.keys if k.src])
+        self.n_targets = len([k for k in self.keys if k.trg])
+
+        # Set dataset size
+        self.size = list(sizes)[0]
 
     def __getitem__(self, idx):
         return {k: self.datasets[k][idx] for k in self.keys}
@@ -122,9 +124,11 @@ class MultimodalDataset(Dataset):
         return self.size
 
     def __repr__(self):
-        s = "{} - ({} sources / {} targets)\n".format(
+        s = "{} - ({} source(s) / {} target(s))\n".format(
             self.__class__.__name__, self.n_sources, self.n_targets)
-        s += "Batches sorted by '{}' lengths\n".format(self.bucket_by)
+        s += "  Sampler type: {}, bucket_by: {}\n".format(
+            self.sampler_type, self.bucket_by)
+
         if self.n_sources > 0:
             s += "  Sources:\n"
             for name in filter(lambda k: k.src, self.keys):
