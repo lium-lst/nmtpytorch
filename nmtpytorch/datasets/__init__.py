2070b33ad5bd769ee06a950da787bba88b5ffd7c
Ozan Caglayan <ozancag@gmail.com>
2018-11-14 19:31:39 +0100

    datasets.__init__: add getter

--- nmtpytorch/datasets/__init__.py
+++ nmtpytorch/datasets/__init__.py
@@ -1,8 +1,25 @@
+# First the basic types
 from .numpy import NumpyDataset
 from .kaldi import KaldiDataset
 from .imagefolder import ImageFolderDataset
 from .text import TextDataset
 from .onehot import OneHotDataset
 from .numpy_sequence import NumpySequenceDataset
+from .label import LabelDataset
 from .shelve import ShelveDataset
+
+# Second the selector function
+def get_dataset(type_):
+    return {
+        'numpy': NumpyDataset,
+        'numpysequence': NumpySequenceDataset,
+        'kaldi': KaldiDataset,
+        'imagefolder': ImageFolderDataset,
+        'text': TextDataset,
+        'onehot': OneHotDataset,
+        'label': LabelDataset,
+        'shelve': ShelveDataset,
+    }[type_.lower()]
+
+# Third the dataset that uses all above
 from .multimodal import MultimodalDataset
