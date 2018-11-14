785f1b452de8cf5386374a585205f2ff7c8bd4e2
Ozan Caglayan <ozancag@gmail.com>
2018-11-14 17:45:45 +0100

    TextEncoder: add layer-norm option, additional projection layer, refactor

--- nmtpytorch/layers/encoders/text.py
+++ nmtpytorch/layers/encoders/text.py
@@ -3,6 +3,7 @@ from torch import nn
 from torch.nn.utils.rnn import pad_packed_sequence, pack_padded_sequence
 
 from ...utils.data import sort_batch
+from .. import FF
 
 
 class TextEncoder(nn.Module):
@@ -24,6 +25,12 @@ class TextEncoder(nn.Module):
             that their norm is the given value.
         emb_gradscale(bool, optional): If `True`, scales the gradients
             per embedding w.r.t. to its frequency in the batch.
+        proj_dim(int, optional): If not `None`, add a final projection
+            layer. Can be used to adapt dimensionality for decoder.
+        proj_activ(str, optional): Non-linearity for projection layer.
+            `None` or `linear` does not apply any non-linearity.
+        layer_norm(bool, optional): Apply layer normalization at the
+            output of the encoder.
 
     Input:
         x (Tensor): A tensor of shape (n_timesteps, n_samples)
@@ -41,7 +48,8 @@ class TextEncoder(nn.Module):
     def __init__(self, input_size, hidden_size, n_vocab, rnn_type,
                  num_layers=1, bidirectional=True,
                  dropout_rnn=0, dropout_emb=0, dropout_ctx=0,
-                 emb_maxnorm=None, emb_gradscale=False):
+                 emb_maxnorm=None, emb_gradscale=False,
+                 proj_dim=None, proj_activ=None, layer_norm=False):
         super().__init__()
 
         self.rnn_type = rnn_type.upper()
@@ -52,6 +60,9 @@ class TextEncoder(nn.Module):
         self.bidirectional = bidirectional
         self.emb_maxnorm = emb_maxnorm
         self.emb_gradscale = emb_gradscale
+        self.proj_dim = proj_dim
+        self.proj_activ = proj_activ
+        self.layer_norm = layer_norm
 
         # For dropout btw layers, only effective if num_layers > 1
         self.dropout_rnn = dropout_rnn
@@ -65,10 +76,8 @@ class TextEncoder(nn.Module):
         if self.bidirectional:
             self.ctx_size *= 2
 
-        if self.dropout_emb > 0:
-            self.do_emb = nn.Dropout(self.dropout_emb)
-        if self.dropout_ctx > 0:
-            self.do_ctx = nn.Dropout(self.dropout_ctx)
+        # Embedding dropout
+        self.do_emb = nn.Dropout(self.dropout_emb)
 
         # Create embedding layer
         self.emb = nn.Embedding(self.n_vocab, self.input_size,
@@ -82,51 +91,42 @@ class TextEncoder(nn.Module):
                        dropout=self.dropout_rnn,
                        bidirectional=self.bidirectional)
 
-    def forward(self, x):
+        output_layers = []
+        if self.layer_norm:
+            output_layers.append(nn.LayerNorm(self.ctx_size))
+        if self.proj_dim:
+            output_layers.append(
+                FF(self.ctx_size, self.proj_dim, activ=self.proj_activ))
+        if self.dropout_ctx > 0:
+            output_layers.append(nn.Dropout(p=self.dropout_ctx))
+        self.output = nn.Sequential(*output_layers)
+
+    def forward(self, x, **kwargs):
+        mask = None
         if (x == 0).nonzero().numel():
-            return self.forward_mixed_len_batches(x)
+            # Non-homogeneous batch
+            # sort the batch by decreasing length of sequences
+            # oidxs: to recover original order
+            # sidxs: idxs to sort the batch
+            # slens: lengths in sorted order for pack_padded_sequence()
+            oidxs, sidxs, slens, mask = sort_batch(x)
+
+            # Fetch embeddings for the sorted batch
+            embs = self.do_emb(self.emb(x[:, sidxs]))
+
+            # Pack and encode
+            packed_emb = pack_padded_sequence(embs, slens)
+
+            # We ignore last_state since we don't use it
+            packed_hs, _ = self.enc(packed_emb)
+
+            # Get hidden states and revert the order
+            hs = pad_packed_sequence(packed_hs)[0][:, oidxs]
         else:
-            return self.forward_same_len_batches(x)
+            # Fetch embeddings
+            embs = self.do_emb(self.emb(x))
 
-    def forward_same_len_batches(self, x):
-        # Fetch embeddings
-        embs = self.emb(x)
+            # Encode
+            hs, _ = self.enc(embs)
 
-        if self.dropout_emb > 0:
-            embs = self.do_emb(embs)
-
-        # Encode
-        hs, _ = self.enc(embs)
-
-        if self.dropout_ctx > 0:
-            hs = self.do_ctx(hs)
-
-        # No mask is returned as batch contains elements of all same lengths
-        return hs, None
-
-    def forward_mixed_len_batches(self, x):
-        # sort the batch by decreasing length of sequences
-        # oidxs: to recover original order
-        # sidxs: idxs to sort the batch
-        # slens: lengths in sorted order for pack_padded_sequence()
-        oidxs, sidxs, slens, mask = sort_batch(x)
-
-        # Fetch embeddings for the sorted batch
-        embs = self.emb(x[:, sidxs])
-
-        if self.dropout_emb > 0:
-            embs = self.do_emb(embs)
-
-        # Pack and encode
-        packed_emb = pack_padded_sequence(embs, slens)
-
-        # We ignore last_state since we don't use it
-        packed_hs, _ = self.enc(packed_emb)
-
-        # Get hidden states and revert the order
-        hs = pad_packed_sequence(packed_hs)[0][:, oidxs]
-
-        if self.dropout_ctx > 0:
-            hs = self.do_ctx(hs)
-
-        return hs, mask
+        return self.output(hs), mask
