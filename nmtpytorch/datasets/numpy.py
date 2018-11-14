b5bf4ca1f867e6c765d5adc6597fec50c812e63e
Ozan Caglayan <ozancag@gmail.com>
2018-11-14 19:30:52 +0100

    NumpyDataset: Critical change for returned batch dim order

--- nmtpytorch/datasets/numpy.py
+++ nmtpytorch/datasets/numpy.py
@@ -15,9 +15,12 @@ class NumpyDataset(Dataset):
             the relevant numpy file.
         key (str, optional): If `fname` is `.npz` file, its relevant `key`
             will be fetched from the serialized object.
+        order_file (str, None): If given, will be used to map sample indices
+            to tensors using this list. Useful for tiled or repeated
+            experiments.
     """
 
-    def __init__(self, fname, key=None):
+    def __init__(self, fname, key=None, order_file=None, **kwargs):
         self.path = Path(fname)
         if not self.path.exists():
             raise RuntimeError('{} does not exist.'.format(self.path))
@@ -28,15 +31,24 @@ class NumpyDataset(Dataset):
             assert key, "A key should be provided for .npz files."
             self.data = np.load(self.path)[key]
 
+        if order_file:
+            with open(order_file) as orf:
+                self.order = [int(x) for x in orf.read().strip().split('\n')]
+        else:
+            self.order = list(range(self.data.shape[0]))
+
         # Dataset size
-        self.size = self.data.shape[0]
+        self.size = len(self.order)
 
     @staticmethod
     def to_torch(batch):
-        return torch.from_numpy(np.array(batch, dtype='float32'))
+        # NOTE: Assumes x.shape == (n, c, *, *, ...)
+        x = torch.from_numpy(np.array(batch, dtype='float32'))
+        # Convert it to (t(=1 if fixed features), n, c)
+        return x.view(*x.size()[:2], -1).permute(2, 0, 1)
 
     def __getitem__(self, idx):
-        return self.data[idx]
+        return self.data[self.order[idx]]
 
     def __len__(self):
         return self.size
