10e796c6ddf60ad30c3a1172888250352819fd26
Ozan Caglayan <ozancag@gmail.com>
2018-12-18 12:41:55 +0100

    layers: add embedding constructor

--- nmtpytorch/layers/__init__.py
+++ nmtpytorch/layers/__init__.py
@@ -5,6 +5,7 @@ from .flatten import Flatten
 from .seq_conv import SequenceConvolution
 from .rnninit import RNNInitializer
 from .max_margin import MaxMargin
+from .embedding import get_partial_embedding_layer
 
 # Attention layers
 from .attention import *
