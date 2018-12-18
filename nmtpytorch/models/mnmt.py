e914f4d4d9fcddc61ef453ff2f3da52882c5da5b
Ozan Caglayan <ozancag@gmail.com>
2018-12-18 12:44:52 +0100

    mnmt: update model

--- nmtpytorch/models/mnmt.py
+++ nmtpytorch/models/mnmt.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
+import torch
 import logging
 
 from .nmt import NMT
+from ..layers import MultimodalTextEncoder
+from ..layers import ConditionalDecoder
 
 logger = logging.getLogger('nmtpytorch')
 
@@ -12,6 +15,7 @@ class MultimodalNMT(NMT):
         Integration types (feat_fusion argument)
             'encinit':      Initialize RNNs in the encoder
             'decinit':      Initializes first decoder RNN.
+            'encdecinit':   Initializes first decoder RNN.
             'vbos':         Replace <bos> in decoder with visual features
             'concat':       Concat the embeddings and features (doubles RNN input)
             'concat_fuse':  Concat the embeddings and features and project
@@ -19,7 +23,6 @@ class MultimodalNMT(NMT):
             'prepend':      Input sequence: [vis, embs, eos]
             'append':       Input sequence: [embs, vis, eos]
             'preappend':    Input sequence: [vis, embs, vis, eos]
-            'vis_replace':  Replaces '<vis>' embedding with features
     """
     def __init__(self, opts):
         super().__init__(opts)
@@ -27,14 +30,31 @@ class MultimodalNMT(NMT):
     def set_defaults(self):
         # Set parent defaults
         super().set_defaults()
+        # NOTE: You should not use dec_init == feats with this model.
+        # Use "feat_fusion:decinit" instead.
         self.defaults.update({
-            'feat_dim': 2048,       # Feature dimension for multimodal encoder
-            'feat_activ': None,     # Feature non-linearity for multimodal encoder
+            'feat_dim': 2048,           # Feature dimension for multimodal encoder
+            'feat_activ': None,         # Feature non-linearity for multimodal encoder
             'feat_fusion': 'encinit',   # By default initialize only the encoder
         })
 
+    def reset_parameters(self):
+        super().reset_parameters()
+        # Reset padding embeddings to 0
+        with torch.no_grad():
+            self.enc.emb.weight.data[0].fill_(0)
+
     def setup(self, is_train=True):
         """Sets up NN topology by creating the layers."""
+        # Hack to sync enc-decinit computation
+        self.dec_requires_img = False
+        if self.opts.model['feat_fusion'] and \
+                'decinit' in self.opts.model['feat_fusion']:
+            self.opts.model['dec_init'] = 'feats'
+            self.opts.model['dec_init_size'] = self.opts.model['feat_dim']
+            self.opts.model['dec_init_activ'] = self.opts.model['feat_activ']
+            self.dec_requires_img = True
+
         self.enc = MultimodalTextEncoder(
             input_size=self.opts.model['emb_dim'],
             hidden_size=self.opts.model['enc_dim'],
@@ -77,14 +97,15 @@ class MultimodalNMT(NMT):
         if self.opts.model['tied_emb'] == '3way':
             self.enc.emb.weight = self.dec.emb.weight
 
+        # Use the same representation everywhere
+        if self.opts.model['feat_fusion'] == 'encdecinit':
+            self.enc.ff_vis.weight = self.dec.ff_dec_init.weight
+
     def encode(self, batch, **kwargs):
-        # BxD auxiliary features
-        aux_feats = batch['feats']
+        d = {str(self.sl): self.enc(batch[self.sl], v=batch.get('feats', None))}
 
-        # By default, there is encoder-side integration
-        d = {str(self.sl): self.enc(batch[self.sl], aux=aux_feats)}
+        # # It may also be decoder-side integration
+        if self.dec_requires_img:
+            d['feats'] = (batch['feats'], None)
 
-        # It may also be decoder-side integration
-        if self.opts.model['dec_init'] == 'feats':
-            d['feats'] = (vis_feats, None)
         return d
