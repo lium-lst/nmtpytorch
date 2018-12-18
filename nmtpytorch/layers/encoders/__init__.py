7c921ba3befe13367de9392a3b617e87bd1a1e58
Ozan Caglayan <ozancag@gmail.com>
2018-12-18 15:17:32 +0100

    encoders: add MultimodalBiLSTMp

--- nmtpytorch/layers/encoders/__init__.py
+++ nmtpytorch/layers/encoders/__init__.py
@@ -3,3 +3,4 @@ from .image import ImageEncoder
 from .text import TextEncoder
 from .bilstmp import BiLSTMp
 from .multimodal_text import MultimodalTextEncoder
+from .multimodal_bilstmp import MultimodalBiLSTMp
