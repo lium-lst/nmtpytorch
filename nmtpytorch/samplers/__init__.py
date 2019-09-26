c330f1512c2c603590fac7552042002027c5f984
Ozan Caglayan <ozancag@gmail.com>
2019-09-26 15:37:44 +0100

    samplers: add helper get_sampler()

--- nmtpytorch/samplers/__init__.py
+++ nmtpytorch/samplers/__init__.py
@@ -1,3 +1,9 @@
 # -*- coding: utf-8 -*-
 from .bucket import BucketBatchSampler
 from .approx import ApproximateBucketBatchSampler
+
+def get_sampler(type_):
+    return {
+        'bucket': BucketBatchSampler,
+        'approximate': ApproximateBucketBatchSampler,
+    }[type_.lower()]
