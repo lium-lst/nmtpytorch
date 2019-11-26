e6d5aff4dc9030318685ebc9aeee38e1bb8eed0b
Ozan Caglayan <ozancag@gmail.com>
2019-11-26 14:59:52 +0000

    layers: Add projected embedding PEmbedding layer type

--- nmtpytorch/layers/embedding.py
+++ nmtpytorch/layers/embedding.py
@@ -4,6 +4,21 @@ import pickle as pkl
 import torch
 from torch import nn
 
+from . import FF
+
+
+class PEmbedding(nn.Embedding):
+    """An extension to regular `nn.Embedding` with MLP and dropout."""
+    def __init__(self, num_embeddings, embedding_dim, out_dim,
+                 activ='linear', dropout=0.0):
+        super().__init__(num_embeddings, embedding_dim, padding_idx=0)
+        self.proj = FF(embedding_dim, out_dim, activ=activ, bias=False)
+        self.do = nn.Dropout(dropout) if dropout > 0.0 else lambda x:x
+
+    def forward(self, input):
+        # Get the embeddings from parent's forward
+        return self.do(self.proj(super().forward(input)))
+
 
 def get_partial_embedding_layer(vocab, embedding_dim, pretrained_file,
                                 freeze='none', oov_zero=True):
