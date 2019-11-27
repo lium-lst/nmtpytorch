ccc9d77d6781857ff55cc6ad5313c5853695b2f8
Ozan Caglayan <ozancag@gmail.com>
2019-11-27 22:15:39 +0000

    cosmetic

--- nmtpytorch/layers/embedding.py
+++ nmtpytorch/layers/embedding.py
@@ -13,7 +13,7 @@ class PEmbedding(nn.Embedding):
                  activ='linear', dropout=0.0):
         super().__init__(num_embeddings, embedding_dim, padding_idx=0)
         self.proj = FF(embedding_dim, out_dim, activ=activ, bias=False)
-        self.do = nn.Dropout(dropout) if dropout > 0.0 else lambda x:x
+        self.do = nn.Dropout(dropout) if dropout > 0.0 else lambda x: x
 
     def forward(self, input):
         # Get the embeddings from parent's forward
