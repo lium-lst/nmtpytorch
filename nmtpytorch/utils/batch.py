c6a8fbce76b6465148ac3a912f6ff2fd1ebbd3a7
Ozan Caglayan <ozancag@gmail.com>
2020-01-16 16:13:44 +0000

    utils.batch: add Batch class

--- nmtpytorch/utils/batch.py
+++ nmtpytorch/utils/batch.py
@@ -1,8 +1,30 @@
 class Batch:
-    """A custom batch object."""
-    def __init__(self, data, device=None):
+    r"""A custom batch object used through data representation.
+
+    Arguments:
+        data(dict): A dictionary with keys representing the data sources
+            as defined by the configuration file and values being the
+            already collated `torch.Tensor` objects for a given batch.
+
+        device(`torch.device`, optional): If given, the batch will be moved
+            to the device right after instance creation.
+
+        non_blocking(bool, optional): If `True`, the tensor copies will
+            use the `non_blocking` argument.
+
+    Notes:
+        In the current code, moving to an appropriate device is handled
+        by an explicit call to `.to()` method from the main loop or
+        other relevant places.
+
+    Returns:
+        a `Batch` instance indexable with bracket notation. The batch size
+        is accessible through the `.size` attribute.
+
+    """
+    def __init__(self, data, device=None, non_blocking=False):
         if device:
-            self.data = {k: v.to(device) for k, v in data.items()}
+            self.data = {k: v.to(device, non_blocking=non_blocking) for k, v in data.items()}
         else:
             self.data = data
 
@@ -14,8 +36,8 @@ class Batch:
     def __getitem__(self, key):
         return self.data[key]
 
-    def to(self, device):
-        self.data.update({k: v.to(device) for k, v in self.data.items()})
+    def to(self, device, non_blocking=False):
+        self.data.update({k: v.to(device, non_blocking=non_blocking) for k, v in self.data.items()})
 
     def __repr__(self):
         s = f"Batch(size={self.size})\n"
