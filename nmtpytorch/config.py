3aa05ddcad2e4938717313e20354d7aed79bd9c5
Ozan Caglayan <ozancag@gmail.com>
2020-01-18 15:41:49 +0000

    Config: change class name, add prepare_script detection

--- nmtpytorch/config.py
+++ nmtpytorch/config.py
@@ -1,66 +1,50 @@
 # -*- coding: utf-8 -*-
-import os
 import sys
 import copy
+import json
+import runpy
 import pathlib
+
 from difflib import get_close_matches
 
 from collections import defaultdict
 
-from configparser import ConfigParser, ExtendedInterpolation
-from ast import literal_eval
-
 
 TRAIN_DEFAULTS = {
-    'num_workers': 0,            # number of workers for data loading (0=disabled)
-    'pin_memory': False,         # pin_memory for DataLoader (Default: False)
-    'seed': 0,                   # > 0 if you want to reproduce a previous experiment
-    'gclip': 5.,                 # Clip gradients above clip_c
-    'l2_reg': 0.,                # L2 penalty factor
-    'patience': 20,              # Early stopping patience
-    'optimizer': 'adam',         # adadelta, sgd, rmsprop, adam
-    'lr': 0.0004,                # 0 -> Use default lr from Pytorch
-    'lr_decay': False,           # Can only be 'plateau' for now
-    'lr_decay_revert': False,    # Return back to the prev best weights after decay
-    'lr_decay_factor': 0.1,      # Check torch.optim.lr_scheduler
-    'lr_decay_patience': 10,     #
-    'lr_decay_min': 0.000001,    #
-    'model_type': '',            # Name of model class to train
-    'momentum': 0.0,             # momentum for SGD
-    'nesterov': False,           # Enable Nesterov for SGD
-    'disp_freq': 30,             # Training display frequency (/batch)
-    'batch_size': 32,            # Training batch size
-    'max_epochs': 100,           # Max number of epochs to train
-    'max_iterations': int(1e6),  # Max number of updates to train
-    'eval_metrics': 'loss',      # comma sep. metrics, 1st -> earlystopping
-    'eval_filters': '',          # comma sep. filters to apply to refs/hyps
-    'eval_beam': 6,              # Validation beam size
-    'eval_batch_size': 16,       # batch_size for beam-search
-    'eval_freq': 3000,           # 0 means 'End of epochs'
-    'eval_max_len': 200,         # max seq len to stop during beam search
-    'eval_start': 1,             # Epoch which validation will start
-    'eval_zero': False,          # Evaluate once before starting training
-                                 # Useful when using pretrained_file
-    'save_best_metrics': True,   # Save best models for each eval_metric
-    'save_path': '',             # Path to root experiment folder
-    'save_optim_state': False,   # Save optimizer states into checkpoint
-    'checkpoint_freq': 5000,     # Periodic checkpoint frequency
-    'n_checkpoints': 5,          # Number of checkpoints to keep
-    'tensorboard_dir': '',       # Enable TB and give global log folder
     'pretrained_file': '',       # A .ckpt file from which layers will be initialized
     'pretrained_layers': '',     # comma sep. list of layer prefixes to initialize
     'freeze_layers': '',         # comma sep. list of layer prefixes to freeze
     'handle_oom': False,         # Skip out-of-memory batches
+    'seed': 0,                   # > 0 if you want to reproduce a previous experiment
+    'save_optim_state': False,   # Save optimizer states into checkpoint
+    #'gclip': 5.,                 # Clip gradients above clip_c
+    #'l2_reg': 0.,                # L2 penalty factor
+    #'patience': 20,              # Early stopping patience
+    #'optimizer': 'adam',         # adadelta, sgd, rmsprop, adam
+    #'lr': 0.0004,                # 0 -> Use default lr from Pytorch
+    #'lr_decay': False,           # Can only be 'plateau' for now
+    #'lr_decay_revert': False,    # Return back to the prev best weights after decay
+    #'lr_decay_factor': 0.1,      # Check torch.optim.lr_scheduler
+    #'lr_decay_patience': 10,     #
+    #'lr_decay_min': 0.000001,    #
+    #'momentum': 0.0,             # momentum for SGD
+    #'nesterov': False,           # Enable Nesterov for SGD
+    #'batch_size': 32,            # Training batch size
+    #'max_epochs': 100,           # Max number of epochs to train
+    #'max_iterations': int(1e6),  # Max number of updates to train
+    #'eval_metrics': 'loss',      # comma sep. metrics, 1st -> earlystopping
+    #'eval_filters': '',          # comma sep. filters to apply to refs/hyps
+    #'eval_beam': 6,              # Validation beam size
+    #'eval_batch_size': 16,       # batch_size for beam-search
+    #'eval_freq': 3000,           # 0 means 'End of epochs'
+    #'eval_max_len': 200,         # max seq len to stop during beam search
+    #'eval_start': 1,             # Epoch which validation will start
+    #'eval_zero': False,          # Evaluate once before starting training
+                                 # Useful when using pretrained_file
+    #'save_best_metrics': True,   # Save best models for each eval_metric
+    #'checkpoint_freq': 5000,     # Periodic checkpoint frequency
+    #'n_checkpoints': 5,          # Number of checkpoints to keep
 }
-
-
-def expand_env_vars(data):
-    """Interpolate some environment variables."""
-    for key in ('HOME', 'USER', 'LOCAL', 'SCRATCH', 'PWD'):
-        var = '$' + key
-        if var in data and key in os.environ:
-            data = data.replace(var, os.environ[key])
-    return data
 
 
 def resolve_path(value):
@@ -73,28 +57,7 @@ def resolve_path(value):
     return value
 
 
-def _parse_value(value):
-    """Automatic type conversion for configuration values.
-
-    Arguments:
-        value(str): A string to parse.
-    """
-
-    # Check for boolean or None
-    if str(value).capitalize().startswith(('False', 'True', 'None')):
-        return eval(str(value).capitalize(), {}, {})
-
-    # Detect strings, floats and ints
-    try:
-        # If this fails, this is a string
-        result = literal_eval(value)
-    except Exception:
-        result = value
-
-    return result
-
-
-class Options:
+class Config:
     @classmethod
     def from_dict(cls, dict_, override_list=None):
         """Loads object from dict."""
@@ -117,45 +80,39 @@ class Options:
             section, keyvalue = opt.split('.', 1)
             key, value = keyvalue.split(':')
             value = resolve_path(value)
-            overrides[section][key] = _parse_value(value)
+            overrides[section][key] = value
         return overrides
 
     def __init__(self, filename, overrides=None):
-        self._parser = ConfigParser(interpolation=ExtendedInterpolation())
         self.filename = filename
-        self.sections = []
-
-        with open(self.filename) as fhandle:
-            data = expand_env_vars(fhandle.read().strip())
-
-        # Read the defaults first
-        self._parser.read_dict({'train': TRAIN_DEFAULTS})
-
-        # Read the config
-        self._parser.read_string(data)
+        self.sections = {
+            'tasks': {},
+            'train': TRAIN_DEFAULTS,
+            'data': {},
+            'vocabulary': {},
+            'optimizers': {},
+            'evaluation': {},
+            'samplers': {},
+        }
 
         if overrides is not None:
             # ex: train.batch_size:32
             self.overrides = self.parse_overrides(overrides)
 
-        for section in self._parser.sections():
-            opts = {}
-            self.sections.append(section)
+        # Run the python file and get the relevant items
+        namespace = runpy.run_path(filename)
+        self.prepare_script = namespace.get('prepare', None)
+        for section in self.sections:
+            self.sections[section].update(namespace[section])
+            self.sections[section].update(self.overrides[section])
+            for key, value in self.sections[section].items():
+                self.sections[section][key] = resolve_path(value)
 
-            for key, value in self._parser[section].items():
-                opts[key] = resolve_path(_parse_value(value))
-
-            if section in self.overrides:
-                for (key, value) in self.overrides[section].items():
-                    opts[key] = value
-
-            setattr(self, section, opts)
-
-        # Sanity check for [train]
-        train_keys = list(self.train.keys())
+        # Sanity check for `train`
+        train_keys = list(self.sections['train'].keys())
         def_keys = list(TRAIN_DEFAULTS.keys())
         assert len(train_keys) == len(set(train_keys)), \
-            "Duplicate arguments found in config's [train] section."
+            "Duplicate arguments found in config's `train` section."
 
         invalid_keys = set(train_keys).difference(set(TRAIN_DEFAULTS))
         for key in invalid_keys:
@@ -167,39 +124,22 @@ class Options:
         if invalid_keys:
             sys.exit(1)
 
+        # Save the internal state for further serialization purposes
+        self._state = self.__set_state()
+
+    @property
+    def state(self):
+        return self._state
+
     def __repr__(self):
-        repr_ = ""
-        for section in self.sections:
-            opts = getattr(self, section)
-            repr_ += "-" * (len(section) + 2)
-            repr_ += "\n[{}]\n".format(section)
-            repr_ += "-" * (len(section) + 2)
-            repr_ += '\n'
-            for key, value in opts.items():
-                if isinstance(value, list):
-                    repr_ += "{:>20}:\n".format(key)
-                    for elem in value:
-                        repr_ += "{:>22}\n".format(elem)
-                elif isinstance(value, dict):
-                    repr_ += "{:>20}:\n".format(key)
-                    for kkey, vvalue in value.items():
-                        repr_ += "{:>22}:{}\n".format(kkey, vvalue)
-                else:
-                    repr_ += "{:>20}:{}\n".format(key, value)
-        repr_ += "-" * 70
-        repr_ += "\n"
-        return repr_
+        return json.dumps(self.sections, indent=2, default=lambda obj: str(obj))
 
-    def to_dict(self):
+    def __set_state(self):
         """Serializes the instance as dict."""
-        dict_ = {
-            'filename': self.filename,
-            'sections': self.sections,
-        }
-        for section in self.sections:
-            dict_[section] = copy.deepcopy(getattr(self, section))
+        dict_ = {}
+        for section, value in self.sections.items():
+            dict_[section] = copy.deepcopy(value)
+        self._state = dict_
 
-        return dict_
-
-    def __getitem__(self, key):
-        return getattr(self, key)
+    def __getattr__(self, key):
+        return self.sections[key]
