b8fe2bdff3044866e93825c28acf34caf6a5619a
Ozan Caglayan <ozancag@gmail.com>
2020-01-20 12:13:19 +0000

    Task: improve

--- nmtpytorch/utils/task.py
+++ nmtpytorch/utils/task.py
@@ -1,9 +1,18 @@
 class Task:
-    def __init__(self, name):
+    """A class which represents a task.
+
+    Arguments:
+        name(str): The task name as defined in the configuration file.
+        params(dict, optional): If not `None`, stores the task-relevant
+            arguments as well.
+
+    """
+    def __init__(self, name, params=None):
         assert name.count(':') == name.count('-') == 1, \
             "Task name should be in <prefix>:<src1,...,srcN>-<trg1,...,trgN> format"
 
         self.name = name
+        self.params = params
         self.prefix, topology = self.name.split(':')
         srcs, trgs = topology.split('-')
         self.sources = srcs.split(',')
@@ -11,5 +20,13 @@ class Task:
         self.multi_source = len(self.sources) > 1
         self.multi_target = len(self.targets) > 1
 
+        self.src, self.trg = None, None
+
+        if not self.multi_source:
+            self.src = self.sources[0]
+
+        if not self.multi_target:
+            self.trg = self.targets[0]
+
     def __repr__(self):
-        return f'Task({self.name}, srcs={self.sources!r}, trgs={self.targets!r}'
+        return f'Task({self.name}, srcs={self.sources!r}, trgs={self.targets!r})'
