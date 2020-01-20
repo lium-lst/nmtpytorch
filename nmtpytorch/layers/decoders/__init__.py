a163ec899e99f4c2a8177d4792202948e1587176
Ozan Caglayan <ozancag@gmail.com>
2020-01-20 12:40:39 +0000

    remove get_decoder

--- nmtpytorch/layers/decoders/__init__.py
+++ nmtpytorch/layers/decoders/__init__.py
@@ -5,13 +5,3 @@ from .multisourceconditional import MultiSourceConditionalDecoder
 from .xu import XuDecoder
 from .switchinggru import SwitchingGRUDecoder
 from .vector import VectorDecoder
-
-
-def get_decoder(type_):
-    """Only expose ones with compatible __init__() arguments for now."""
-    return {
-        'cond': ConditionalDecoder,
-        'simplegru': SimpleGRUDecoder,
-        'condmm': ConditionalMMDecoder,
-        'vector': VectorDecoder,
-    }[type_]
